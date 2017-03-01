e3db7a2a4b6405a006da05016e227aa1517e3ef8
Alex Pestell <alex.pestell@gmail.com>
2017-03-02 00:00:36 +0100

    Add transporter off lights

--- src/components/Transporter.js
+++ src/components/Transporter.js
@@ -4,7 +4,15 @@ class Transporter extends React.Component {
 	render() {
 		return (
 			<div className="transporterContainer">
-				<div className="transporter">
+				<div className="transporterOff"><div className="transportLight"><div className="hilight"></div></div></div>
+				<div className="transporterOff"><div className="transportLight"><div className="hilight"></div></div></div>
+				<div className="transporterOff"><div className="transportLight"><div className="hilight"></div></div></div>
+				<div className="transporterOff"><div className="transportLight"><div className="hilight"></div></div></div>
+				<div className="transporterOff"><div className="transportLight"><div className="hilight"></div></div></div>
+				<div className="transporterOff"><div className="transportLight"><div className="hilight"></div></div></div>
+				<div className="transporterOff"><div className="transportLight"><div className="hilight"></div></div></div>
+				<div className="transporterOff"><div className="transportLight"><div className="hilight"></div></div></div>
+				<div className="transporter">					
 					<div className="transportLight transportLightActive" style={{left: this.props.pos * 100 + 'px'}}><div className="hilight"></div></div>
 				</div>
 			</div>
